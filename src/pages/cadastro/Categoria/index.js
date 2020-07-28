32606ec8171fe9130ef848b2a0b3b82b34981428
bllackdev <rapha_rmm@hotmail.com>
2020-07-28 12:05:37 -0300

    add 'Página em Construção'

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import PageDefault from '../../../components/PagesDefault';
 import { Link } from 'react-router-dom';
+import PaginaConstrucao from '../../../assets/img/PaginaConstrucao.png';
 
 function CadastroCategoria() {
   return (
     <PageDefault>
+      <a href="/">
+        <img
+          className="PaginaConstrucao"
+          src={PaginaConstrucao}
+          alt="Página em Construção"
+        />
+      </a>
+
       <h1>Cadastro de Categoria</h1>
 
       <form>
--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import PageDefault from '../../../components/PagesDefault';
 import { Link } from 'react-router-dom';
+import PaginaConstrucao from '../../../assets/img/PaginaConstrucao.png';
 
 function CadastroVideo() {
   return (
     <PageDefault>
+      <a href="/">
+        <img
+          className="PaginaConstrucao"
+          src={PaginaConstrucao}
+          alt="Página em Construção"
+        />
+      </a>
       <h1>Cadastro de Video</h1>
       <Link to="/cadastro/categoria">Cadastrar Categoria</Link>
     </PageDefault>
