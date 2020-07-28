c452838baf4a4968129d784d31651ed59236e934
bllackdev <rapha_rmm@hotmail.com>
2020-07-27 23:29:20 -0300

    Atualizar conteudo no Carousel

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import Logo from './../../assets/img/Logo.png';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="/">
+        <img className="Logo" src={Logo} alt="BllackFlixDev logo" />
       </a>
       <p>
-        Orgulhosamente criado durante a
-        {' '}
-        <a href="https://www.alura.com.br/">
+        Criado por{' '}
+        <a href="https://github.com/bllackdev" target="_blank">
+          BllackDev
+        </a>{' '}
+        <br />
+        durante a{' '}
+        <a href="https://www.alura.com.br/imersao-react" target="_blank">
           Imersão React da Alura
         </a>
       </p>
--- src/components/Menu/components/ButtonLink/index.js
+++ src/components/Menu/components/ButtonLink/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ButtonLink(props) {
   // props => { className: " conteudo que for passar", href: "/"}
-  console.log(props);
+
   return (
     <a href={props.href} className={props.className}>
       {props.children}
