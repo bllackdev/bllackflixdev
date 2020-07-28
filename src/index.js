0c63c5c987a5e465c96b56066413146a4e22a8ac
bllackdev <rapha_rmm@hotmail.com>
2020-07-28 10:40:34 -0300

    add page Cadastro de Video

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+
+const Error404 = () => <div>Error 404</div>;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    {/* SWITCH: COMO SE FOSSE O 'IF', PARA ESCOLHER A PAGINA QUE SERÁ CARREGADA */}
+    <Switch>
+      {/* ROUTE: INDICA A ROTA E O COMPONENTE QUE SERÁ ACESSADO */}
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} exact />
+      <Route component={Error404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
---
+++ src/pages/cadastro/Video/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PageDefault from '../../../components/PagesDefault';
+
+function CadastroVideo() {
+  return <PageDefault>Página de Cadastro de Video</PageDefault>;
+}
+
+export default CadastroVideo;
