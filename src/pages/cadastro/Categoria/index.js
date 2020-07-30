4ebe9cf1a6a5a425fcdb1e9e891920edc0f0c7e1
bllackdev <rapha_rmm@hotmail.com>
2020-07-30 16:35:22 -0300

    Add Eslint, PropType e estilização Categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,15 +1,18 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable linebreak-style */
 import React, { useState } from 'react';
-import PageDefault from '../../../components/PagesDefault';
 import { Link } from 'react-router-dom';
-import FormField from '../../../components/FormField'
-import PaginaConstrucao from '../../../assets/img/PaginaConstrucao.png';
+import PageDefault from '../../../components/PagesDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
   const valoresIniciais = {
     nome: '',
     descricao: '',
-    cor: ''
-  }
+    cor: '',
+  };
   // COCHETES ANTES DO '=', VOCE ESTÁ ABRINDO O ARRAY PARA COLETAR VALORES
   const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valoresIniciais);
@@ -19,31 +22,35 @@ function CadastroCategoria() {
     setValues({
       ...values,
       [chave]: valor, // nome: 'valor'
-    })
+    });
   }
 
   function handleChange(infosDoEvento) {
     const { getAttribute, value } = infosDoEvento.target;
     setValue(
       getAttribute('name'),
-      value
+      value,
     );
   }
 
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria: {values.nome}</h1>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
 
       <form onSubmit={function handleSubmit(infosDoEvento) {
         infosDoEvento.preventDefault();
         setCategorias([
           // '...' ESTÁ ESPALHANDO OS VALORES
           ...categorias,
-          values
+          values,
         ]);
 
-        setValues(valoresIniciais)
-      }}>
+        setValues(valoresIniciais);
+      }}
+      >
 
         <FormField
           label="Nome da Categoria: "
@@ -55,22 +62,11 @@ function CadastroCategoria() {
 
         <FormField
           label="Descrição: "
-          type="???"
+          type="textarea"
           value={values.descricao}
           name="descricao"
           onChange={handleChange}
         />
-        {/* <div>
-          <label>
-            Descrição:
-              <textarea
-              type="text"
-              value={values.descricao}
-              name="descricao"
-              onChange={handleChange}
-            />
-          </label>
-        </div> */}
 
         <FormField
           label="Cor: "
@@ -79,29 +75,16 @@ function CadastroCategoria() {
           name="cor"
           onChange={handleChange}
         />
-        {/* <div>
-          <label>
-            Cor:
-            <input
-              type="color"
-              value={values.cor}
-              name="cor"
-              onChange={handleChange}
-            />
-          </label>
-        </div> */}
 
-        <button>Cadastrar</button>
+        <Button>Cadastrar</Button>
       </form>
 
       <ul>
-        {categorias.map((categoria, indice) => {
-          return (
-            <li key={`${categoria}${indice}`}>
-              {categoria}
-            </li>
-          )
-        })}
+        {categorias.map((categoria, indice) => (
+          <li key={`${categoria}${indice}`}>
+            {categoria}
+          </li>
+        ))}
       </ul>
 
       <Link to="/">Ir para Home</Link>
