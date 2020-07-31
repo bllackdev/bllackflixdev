e5d979eeb9feef0d2084813e6c7834eae63724f4
bllackdev <rapha_rmm@hotmail.com>
2020-07-30 23:24:15 -0300

    add BackEnd2

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -32,6 +32,21 @@ function CadastroCategoria() {
     );
   }
 
+  // useEffect(() => {
+  //   //   if (window.location.href.includes('localhost')) {
+  //   //     const URL = 'http://localhost:8080/categorias';
+  //   //     fetch(URL)
+  //   //       .then(async (respostaDoServer) => {
+  //   //         if (respostaDoServer.ok) {
+  //   //           const resposta = await respostaDoServer.json();
+  //   //           setCategorias(resposta);
+  //   //           return;
+  //   //         }
+  //   //         throw new Error('Não foi possível pegar os dados');
+  //   //       });
+  //   //   }
+  //   // }, []);
+
   useEffect(() => {
     const URL_TOP = window.location.hostname.includes('localhost')
       ? 'http://localhost:8080/categorias'
